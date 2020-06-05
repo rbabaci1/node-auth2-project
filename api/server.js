03e647b199a59ef53e3fee1d1ba0806fbb57f61f
rabah babaci <4rabah@gmail.com>
2020-06-05 12:30:14 -0700

    create the auth router

--- api/server.js
+++ api/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
+const usersRouter = require("../users/users-router");
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "API is up!" });
