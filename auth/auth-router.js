65139be6a9a41e21751bc5fd15b78d8d881a016d
rabah babaci <4rabah@gmail.com>
2020-06-05 13:50:00 -0700

    implement the post new user endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const secrets = require("../utils/secrets");
 const validateUserCredentials = require("../utils/validation-middleware");
 const Users = require("../users/users-model");
+const generateToken = require("../utils/generateToken");
 
 router.post("/register", validateUserCredentials, async (req, res, next) => {
   try {
@@ -12,8 +13,9 @@ router.post("/register", validateUserCredentials, async (req, res, next) => {
     const hash = bcrypt.hashSync(user.password, rounds);
 
     const addedUser = await Users.add({ ...user, password: hash });
+    const token = generateToken(addedUser);
 
-    req.statusCode(201).json({ message: "" });
+    req.statusCode(201).json({ addedUser, token });
   } catch ({ errno, code, message }) {
     next({
       message: "The recipe could not be added at this moment.",
