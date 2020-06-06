2db9c9addfc5a6814b29b698d84d1656e63723d7
rabah babaci <4rabah@gmail.com>
2020-06-05 19:53:34 -0700

    reset database

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -4,20 +4,19 @@ const secrets = require("../utils/secrets");
 
 module.exports = (req, res, next) => {
   try {
-    const [directive, token] = req.headers.authorization.split(" ");
+    const authorization = req.headers.authorization;
+    const [directive, token] = authorization ? authorization : [false, false];
 
     if (directive !== "bearer") {
       res
         .status(401)
         .json({ message: "Please provide token type in headers." });
-    }
-    if (token) {
+    } else if (token) {
       jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
         if (err) {
           res.status(401).json({ message: "Invalid credentials, try again?" });
         } else {
           req.decodedJwt = decodedToken;
-          console.log(decodeToken);
           next();
         }
       });
@@ -26,7 +25,7 @@ module.exports = (req, res, next) => {
     }
   } catch ({ message, statusCode }) {
     next({
-      message: "Can't authenticate this user at this moment.",
+      message: "User can not be authenticated at this moment.",
       reason: message,
       statusCode,
     });
