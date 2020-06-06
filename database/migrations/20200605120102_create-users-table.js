dda40bffee243d3076bb3be16c5579aaf06da716
rabah babaci <4rabah@gmail.com>
2020-06-06 10:56:40 -0700

    add two colomuns to the users table

--- database/migrations/20200605120102_create-users-table.js
+++ database/migrations/20200605120102_create-users-table.js
@@ -1,6 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", tbl => {
     tbl.increments();
+
+    tbl.string("firstName", 128).notNullable();
+    tbl.string("lastName", 128).notNullable();
     tbl.string("username", 128).notNullable().unique();
     tbl.string("password", 128).notNullable();
     tbl.string("department", 128).notNullable();
--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -5,6 +5,8 @@ exports.seed = knex => {
       knex("users").insert([
         {
           id: 1,
+          firstName: "Rabah",
+          lastName: "Babaci",
           username: "rabah_babaci",
           password: "rabah",
           department: "admin",
