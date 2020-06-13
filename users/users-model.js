ea18f5a0fe7d142ce439b64b1b49698dbbea6842
rabah babaci <4rabah@gmail.com>
2020-06-12 19:35:31 -0700

    update the add users model

--- users/users-model.js
+++ users/users-model.js
@@ -3,7 +3,7 @@ const db = require("../database/dbConfig");
 const findBy = filter => db("users").where(filter).first();
 
 const add = async newUser => {
-  const [id] = await db("users").insert(newUser);
+  const [id] = await db("users").insert(newUser, "id");
 
   return findBy({ id });
 };
