b30d49e624f493ce430e2b837709cc4219f1ea27
rabah babaci <4rabah@gmail.com>
2020-06-12 18:10:20 -0700

    re-run the testing database seeds

--- users/users-model.test.js
+++ users/users-model.test.js
@@ -1,18 +1,12 @@
 const db = require("../database/dbConfig");
 const Users = require("./users-model");
-
-const mockUser = {
-  firstName: "Kyla",
-  lastName: "Gifford",
-  username: "kyla1997",
-  password: "kyla",
-  department: "sales",
-};
+const mockData = require("../utils/mockData");
 
 describe("users model", () => {
   describe("add()", () => {
     it("should insert a user into the DB", async () => {
-      const insertedUser = await Users.add(mockUser);
+      await Users.add(mockData[0]);
+      await Users.add(mockData[1]);
     });
   });
 });
