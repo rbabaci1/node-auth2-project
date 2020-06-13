7805eb975a57df5d05192846aeed4142d1aa4c6c
rabah babaci <4rabah@gmail.com>
2020-06-12 18:22:35 -0700

    add a test for the add database model

--- users/users-model.test.js
+++ users/users-model.test.js
@@ -1,12 +1,28 @@
 const db = require("../database/dbConfig");
 const Users = require("./users-model");
+
 const mockData = require("../utils/mockData");
 
 describe("users model", () => {
   describe("add()", () => {
+    beforeEach(async () => {
+      await db("users").truncate();
+    });
+
     it("should insert a user into the DB", async () => {
       await Users.add(mockData[0]);
       await Users.add(mockData[1]);
+
+      const users = await db("users");
+      expect(users).toHaveLength(2);
+    });
+
+    it("should return the inserted user", async () => {
+      let insertedUser = await Users.add(mockData[0]);
+      expect(insertedUser.firstName).toBe("Kyla");
+
+      insertedUser = await Users.add(mockData[1]);
+      expect(insertedUser.firstName).toBe("Nancy");
     });
   });
 });
