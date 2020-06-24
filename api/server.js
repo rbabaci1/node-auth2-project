9466ac71070a2d6af83e75dd845ae1547dc04dd8
rabah babaci <4rabah@gmail.com>
2020-06-24 10:07:57 -0700

    clean tests

--- api/server.js
+++ api/server.js
@@ -8,21 +8,11 @@ const usersRouter = require("../users/users-router");
 const authRouter = require("../auth/auth-router");
 
 server.use(express.json());
-// server.use(helmet());
+server.use(helmet());
 server.use(cors());
 
 server.use("/api/users", usersRouter);
 server.use("/api/auth", authRouter);
-
-server.post("/api/recommend", async (req, res) => {
-  try {
-    const data = req.body;
-    const res = await Axios.post("https://medicabi.herokuapp.com/send", data);
-    res.status(200).json(res.data);
-  } catch (error) {
-    res.status(500).json({ message: "Catch block" });
-  }
-});
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "*** API is up! ***" });
