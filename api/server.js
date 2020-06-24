baab43f65b560e7efeb0724594e8f8956348e3fd
rabah babaci <4rabah@gmail.com>
2020-06-24 10:05:04 -0700

    heroku test

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const usersRouter = require("../users/users-router");
 const authRouter = require("../auth/auth-router");
 
 server.use(express.json());
-server.use(helmet());
+// server.use(helmet());
 server.use(cors());
 
 server.use("/api/users", usersRouter);
@@ -16,12 +16,9 @@ server.use("/api/auth", authRouter);
 
 server.post("/api/recommend", async (req, res) => {
   try {
-    console.log(req.body);
-    res.status(200).json({ message: "made it!" });
-    // const data = req.body;
-    // const res = await Axios.post("https://medicabi.herokuapp.com/send", data);
-    // console.log(res);
-    // res.status(200).json(res)
+    const data = req.body;
+    const res = await Axios.post("https://medicabi.herokuapp.com/send", data);
+    res.status(200).json(res.data);
   } catch (error) {
     res.status(500).json({ message: "Catch block" });
   }
